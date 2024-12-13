7993960cd66d4f2266ae5d65704284afce88cb60
karolp-02 <karol200235@wp.pl>
2024-12-13 20:03:37 +0100

    particle i poprawka ui

--- Assets/codes/Collectible.cs
+++ Assets/codes/Collectible.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    [SerializeField]
+    private ParticleSystem collectParticle;
     private AudioSource collectSound;
     // Start is called before the first frame update
     void Start()
     {
+        collectParticle = GetComponentInChildren<ParticleSystem>();
         collectSound = GameObject.Find("pickup").GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider collision)
     {
+        collectParticle.transform.parent = null; //odlaczam particle system od prefabu gracza aby nie znikal razem z nim
+        collectParticle.Play();
         collision.gameObject.GetComponent<MovementController>().collectScore();
         collectSound.Play();
         Debug.Log("Zdobyto punkt");
